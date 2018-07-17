3de937a2b160a65a11fec32a04f7e98b76ddf5a8
alex00321 <alex00321@126.com>
2018-07-17 10:57:16 +0800

    Update coroweb.py

--- awesome-python3-webapp/www/coroweb.py
+++ awesome-python3-webapp/www/coroweb.py
@@ -15,7 +15,7 @@ def get(path):
         def wrapper(*args,**kw):
             return func(*args,**kw)
         wrapper.__method__='GET'
-        wrapper.__route_path
+        wrapper.__route__=path
         return wrapper
     return decorator
 
@@ -54,7 +54,7 @@ def has_named_kw_args(fn):
         if param.kind==inspect.Parameter.KEYWORD_ONLY:
             return True
 
-def has_var_kw_args(fn):
+def has_var_kw_arg(fn):
     params=inspect.signature(fn).parameters
     for name,param in params.items():
         if param.kind==inspect.Parameter.VAR_KEYWORD:
@@ -62,7 +62,7 @@ def has_var_kw_args(fn):
 
 def has_request_arg(fn):
     sig=inspect.signature(fn)
-    params=sig.paramters
+    params=sig.parameters
     found=False
     for name,param in params.items():
         if name=='request':
@@ -135,34 +135,33 @@ class RequestHandler(object):
         except APIError as e:
             return dict(error=e.error,data=e.data,message=e.message)
     
-    def add_static(app):
-        path=os.path.join(os.path.dirname(os.path.abspath(__file__)),'static')
-        app.router.add_static('/static/',path)
-        logging.info('add static%=>%s' %('/static/',path))
-    def add_route(app,fn):
-        method=getattr(fn,'__method__',None)
-        path=getattr(fn,'__route__',None)
-        if path is None or method is None:
-            raise ValueError('@get or @post not defined in %s.'%str(fn))
-        if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
-            fn =asyncio.coroutine(fn)
-        logging.info('add route %s%s =>%s(%s)'%(method,path,fn.__name__,','.join(inpect.signature(fn).parameters.keys())))
-        app.router.add_route(method,path,RequestHandler(app,fn))
-    def add_routes(app,module_name):
-        n=module_name.rfind('.')
-        if n==(-1):
-            mod=__import__(module_name,globals(),locals())
-        else:
-            name=module_name[n+1:]
-            mod=getattr(__import__(module_name[:n],globals(),locals(),[name]),name)
-            for attr in dir(mod):
-                if attr.startswith('_'):
-                    continue
-                fn=getattr(mod,attr)
-                if callable(fn):
-                    method=getattr(fn,'__method__',None)
-                    path=getattr(fn,'__route__',None)
-                    if method and path:
-                        add_route(app,fn)
+def add_static(app):
+    path=os.path.join(os.path.dirname(os.path.abspath(__file__)),'static')
+    app.router.add_static('/static/',path)
+    logging.info('add static %s => %s' %('/static/',path))
+def add_route(app,fn):
+    method=getattr(fn,'__method__',None)
+    path=getattr(fn,'__route__',None)
+    if path is None or method is None:
+        raise ValueError('@get or @post not defined in %s.'%str(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+        fn =asyncio.coroutine(fn)
+    logging.info('add route %s%s =>%s(%s)'%(method,path,fn.__name__,','.join(inspect.signature(fn).parameters.keys())))
+    app.router.add_route(method,path,RequestHandler(app,fn))
+def add_routes(app,module_name):
+    n=module_name.rfind('.')
+    if n==(-1):
+        mod=__import__(module_name,globals(),locals())
+    else:
+        name=module_name[n+1:]
+        mod=getattr(__import__(module_name[:n],globals(),locals(),[name]),name)
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        fn=getattr(mod,attr)
+        if callable(fn):
+            method=getattr(fn,'__method__',None)
+            path=getattr(fn,'__route__',None)
+            if method and path:
+                add_route(app,fn)
     
-        
