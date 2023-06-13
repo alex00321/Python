4bca31e6961c6278c208b752284dde509a5e38eb
alex00321 <alex00321@126.com>
2023-06-14 07:27:14 +0800

    committing self learning

--- Self_learning/selflearning.py
+++ Self_learning/selflearning.py
@@ -1,42 +1,13 @@
-import time, threading
+# sample_str = "today is sunday"
+# i = []
 
-# #This is the first practice
-# def loop():
+# updated_str = sample_str.split(" ")
+# for item in updated_str:
+#     if len(item)>3:
+#         i.append(item)
 
-#     print(f"thread {threading.current_thread().name} is running..")
-#     n = 0
-#     while n<5:
-#         n+=1
-#         print(f'thread {threading.current_thread().name} >>> {n}')
-#         time.sleep(1)
-    
-#     print(f'thread {threading.current_thread().name} ended.')
+# print(i)
+d = {"mike":10,"lucky":5,"bob":30}
 
-# print(f"thread {threading.current_thread().name} is running..")
-# t = threading.Thread(target=loop, name='LoopThread')
-# t.start()
-# t.join()
-# print(f"thread {threading.current_thread().name} ended.")
-
-balance = 0
-def change_it(n):
-
-    global balance
-    balance = balance+n
-    balance = balance-n
-
-def run_thread(n):
-    for i in range(20000000):
-        change_it(n)
-
-t1 = threading.Thread(target=run_thread(5))
-t2 = threading.Thread(target=run_thread(8))
-#t1 = threading.Thread(target=run_thread, args=(5,))
-#t2 = threading.Thread(target=run_thread, args=(8,))
-
-t1.start()
-t2.start()
-t1.join()
-t2.join()
-print(f"balance is {balance}")
-
+updated_d = map(sorted(d.keys()))
+print(updated_d)
